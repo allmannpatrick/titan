311d03ba30011ef819b75ecb4703b229cedd6642
Patrick <allmannpatrick@gmail.com>
2021-07-21 11:31:11 -0700

    gitignore

--- titan/settings.py
+++ titan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x1w&3p^i!i0p&5j2@k1o_&3tf-sfen7oojc*uzjdmoy=7xxfu%'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
